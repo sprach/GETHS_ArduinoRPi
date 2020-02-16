bc684b3f7058fe0b76c33c56f781c38facc869db
sprach <sprach21@daum.net>
2020-02-17 00:29:44 +0900

    Update 08_ir_sensor3.ino

--- src/ARDUINOTECHNIC/TestModules/08_ir_sensor3/08_ir_sensor3.ino
+++ src/ARDUINOTECHNIC/TestModules/08_ir_sensor3/08_ir_sensor3.ino
@@ -1,13 +1,13 @@
 #include "CheckPinStatus.h"
 
-void callback_ir_sensor(int);
+void callback_check_pin(int);
 
 // 핀 번호 정의
 #define PIN_SW          2
 #define PIN_IR_SENSOR  12
 
-CheckPinStatus _chkSw = CheckPinStatus(PIN_SW, callback_ir_sensor, HIGH);
-CheckPinStatus _chkIrSensor = CheckPinStatus(PIN_IR_SENSOR, callback_ir_sensor, HIGH);
+CheckPinStatus _chkSw = CheckPinStatus(PIN_SW, callback_check_pin, HIGH);
+CheckPinStatus _chkIrSensor = CheckPinStatus(PIN_IR_SENSOR, callback_check_pin, HIGH);
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,10 +25,10 @@ void loop() {
   _chkIrSensor.CheckPin();
 }
 
-//----------------------------------------------------[ callback_ir_sensor() ]
-// IR 센서 신호 입력 상태 검사
+//----------------------------------------------------[ callback_check_pin() ]
+// 입력 핀의 상태 검사
 // 인수에 핀번호가 들어온다.
-void callback_ir_sensor(int pinNo)
+void callback_check_pin(int pinNo)
 {
   Serial.print("Interrupted pin no: ");
   Serial.println(pinNo);
